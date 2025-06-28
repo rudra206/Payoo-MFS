191b8efb2844502390a25a21a3b9a616285d36a8
rura206 <rudradipta34@gmail.com>
2025-06-28 16:10:21 +0600

    coment

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,7 +3,8 @@
  * make sure to preventDefult so that page doesn't reloads
  * 2. get the money user wants to add
  * also get the pin number provider
- * 3.
+ * 3.verify the pin number for worning pin number ==> failed to add
+ * for right pin number,allow to add
  * 
  * 
  */
