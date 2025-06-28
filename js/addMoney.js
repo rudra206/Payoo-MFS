7ae2e13ffad82668f0bfb347b893ee6489003117
rura206 <rudradipta34@gmail.com>
2025-06-28 16:07:07 +0600

    coment

---
+++ js/addMoney.js
@@ -0,0 +1,9 @@
+/**
+ * 1. add event listener to the add Money button (form submit)
+ * make sure to preventDefult so that page doesn't reloads
+ * 2. get the money user wants to add
+ * also get the pin number provider
+ * 3.
+ * 
+ * 
+ */
