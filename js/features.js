93ebea2c3a4674d9b238bba0d34c3bfece57a1e4
rura206 <rudradipta34@gmail.com>
2025-07-01 15:36:06 +0600

    done

--- js/features.js
+++ js/features.js
@@ -1,1 +1,22 @@
-console.log('added');
+// shiw the cash out form 
+document.getElementById('btn-show-cash-out') 
+.addEventListener('click',function(){
+  
+    //show cash out button clicked
+    console.log('show cash out button clicked');
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    
+    // hide the add money form
+    document.getElementById('add-money-form').classList.add('hidden');
+
+})
+
+//show add money form and hide cash out form
+document.getElementById('btn-show-add-money').addEventListener('click',function(){
+    //show add money button clicked
+
+    document.getElementById('add-money-form').classList.remove('hidden');
+    
+    //hide the  acsh out form
+    document.getElementById('cash-out-form').classList.add('hidden');
+})
